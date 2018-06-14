7ec44e169a03fbac74fa46b81a09ad86e919d03d
lucacoppiardi <29088675+lucacoppiardi@users.noreply.github.com>
2018-06-14 15:31:58 +0200

    Tolto logo dallo slideshow

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	head();
 	topbar("home");
 	
-?>
+	echo "
 	<style>
 		.slideshow_container {
 			align-items: center;
@@ -26,10 +26,6 @@
 			.slideshow_container_mobile {
 				display: none;
 			}
-			.prima_img {
-				width: 300px !important;
-				height: 300px !important;
-			}
 		}
 		@media screen and (max-width: 800px) {
 			.slideshow_container {
@@ -42,82 +38,78 @@
 				width: 100%;
 				height: 240px;
 			}
-			.prima_img_mobile {
-				width: 100px !important;
-				height: 100px !important;
-			}
 		}
-	</style>
+	</style>";
 	
-	<?php
-		$files = glob("media/foto/*.*");
-		$nomi = array();
-		for ($i=1; $i<count($files); $i++) {
-			$nomi[$i] = $files[$i];
-		}
-		$nomi[0] = "media/logo.png";
-		//print_r($nomi);
-		echo "<script>";
-		$js_array = json_encode($nomi);
-		echo "var javascript_array = ". $js_array . ";\n";
-		echo "
-		var i = 0;
-		var len = ".(count($nomi)-1).";
-		function gira(verso) {
-			if (verso == 'sx') {
-				if (i>0) i--;
-				document.getElementById('immagine').src = javascript_array[i];
-			}
-			if (verso == 'dx') {
-				if (i<len) {
-					i++;
-				} else {
-					i=0;
-					document.getElementById('immagine').className = 'prima_img';
-				}
-				document.getElementById('immagine').src = javascript_array[i];
-			}
-			document.getElementById('immagine').className = '';
-			if (i==0) {
-				document.getElementById('immagine').className = 'prima_img';
-			}
-		}
-		function gira_mobile(verso) {
-			if (verso == 'sx') {
-				if (i>0) i--;
-				document.getElementById('immagine_mobile').src = javascript_array[i];
-			}
-			if (verso == 'dx') {
-				if (i<len) {
-					i++;
-				} else {
-					i=0;
-					document.getElementById('immagine_mobile').className = 'prima_img';
-				}
-				document.getElementById('immagine_mobile').src = javascript_array[i];
-			}
-			document.getElementById('immagine_mobile').className = '';
-			if (i==0) {
-				document.getElementById('immagine_mobile').className = 'prima_img_mobile';
-			}
-		}
-		</script>";
-	?>
+	$files = glob("media/foto/*.*");
+	$nomi = array();
+	for ($i=0; $i<count($files); $i++) {
+		$nomi[$i] = $files[$i];
+	}
+	$arr = json_encode(print_r($nomi, true));
+	echo "<script>console.log('$arr');</script>";
 	
+	echo "<script>";
+	$js_array = json_encode($nomi);
+	echo "var javascript_array = ". $js_array . ";\n";
+	echo "
+	var i = 0;
+	var len = ".(count($nomi)-1).";
+	function gira(verso) {
+		console.log('in '+i);
+		if (verso == 'sx') {
+			if (i>0) {
+				i--;
+			} else {
+				i = len;
+			}
+			document.getElementById('immagine').src = javascript_array[i];
+		}
+		if (verso == 'dx') {
+			if (i<len) {
+				i++;
+			} else {
+				i=0;
+			}
+			document.getElementById('immagine').src = javascript_array[i];
+		}
+		console.log('out '+i);
+	}
+	function gira_mobile(verso) {
+		if (verso == 'sx') {
+			if (i>0) {
+				i--;
+			} else {
+				i = len;
+			}
+			document.getElementById('immagine_mobile').src = javascript_array[i];
+		}
+		if (verso == 'dx') {
+			if (i<len) {
+				i++;
+			} else {
+				i=0;
+			}
+			document.getElementById('immagine_mobile').src = javascript_array[i];
+		}
+	}
+	</script>";
+	
+	echo "
 	<div class='slideshow_container'>
-		<button class='bottone' onClick="gira('sx');">&lt;</button>
-		<img id='immagine' class='prima_img' src='media/logo.png'>
-		<button class='bottone' onClick="gira('dx');">&gt;</button>
+		<button class='bottone' onClick=\"gira('sx');\">&lt;</button>
+		<img id='immagine' src='$nomi[0]'>
+		<button class='bottone' onClick=\"gira('dx');\">&gt;</button>
 	</div>
 	
 	<div class='slideshow_container_mobile'>
-		<img id='immagine_mobile' class='prima_img_mobile' src='media/logo.png'>
+		<img id='immagine_mobile' src='$nomi[0]'>
 		<br>
-		<button class='bottoneAllineatoColorato' onClick="gira_mobile('sx');">&lt;</button>
-		<button class='bottoneAllineatoColorato' onClick="gira_mobile('dx');">&gt;</button>
+		<button class='bottoneAllineatoColorato' onClick=\"gira_mobile('sx');\">&lt;</button>
+		<button class='bottoneAllineatoColorato' onClick=\"gira_mobile('dx');\">&gt;</button>
 	</div>
+	";
 	
-	
-<?php
 	tail();
+
 ?>
